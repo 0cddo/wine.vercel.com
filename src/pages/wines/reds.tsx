00da6ce8702145e6d79f0e26babaea5ddcbd661f
0cddo <0.c.dddo@gmail.com>
2021-12-23 19:19:36 +0900

    ➕ Emotion 추가

--- src/pages/wines/reds.tsx
+++ src/pages/wines/reds.tsx
@@ -1,8 +1,28 @@
 import type { NextPage } from "next";
+import { Error, Loading, WineCard } from "../../components";
+import { useWineData } from "../../hooks/useWineData";
+import { Wine } from "../../types/Wine";
+
 const RedsWinePage: NextPage = () => {
+  const name = "reds";
+  const { data, error } = useWineData(name);
+
+  if (error) return <Error />;
+  if (!data) return <Loading />;
+
   return (
     <div>
-      <h1>Reds Wines</h1>
+      <h1>Reds Wine</h1>
+      <main>
+        {data.map((wineData: Wine) => {
+          return (
+            <WineCard
+              key={`red-wine-list-${wineData.id}`}
+              wineData={wineData}
+            />
+          );
+        })}
+      </main>
     </div>
   );
 };
